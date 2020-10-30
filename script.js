c2e23437a99c56f9f1fd6e380a749cbcb4ebdb11
lilgujj <gustavkarlandersson@hotmail.com>
2020-10-30 10:39:28 +0100

    kkkk

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 
 
+
+
 function calculateSum() {
+
+
     const totalSumma = parseInt(document.getElementById("totalSumma").value);
     const betaldSumma = parseInt(document.getElementById("betaldSumma").value);
     let calculate = betaldSumma - totalSumma;
 
-    const result = document.getElementById("result");
+    let result = document.getElementById("result");
 
+    let cash = [100, 50, 20, 10, 5, 1];
+    for (i = 0; i < cash.length; i++) {
+          
+    }
 
  
     if (isNaN(totalSumma, betaldSumma)) {
         alert("enter a valid number")
-    } else {
-     result.innerText = "du skall få  tillbaka" + " " + calculate + " " + "kr";
- 
     }
+    else {
+     result.innerText = "du skall få  tillbaka" + " " + calculate + " " + "kr";
+    }
+
+
  }
 
-   
+
+
+
 
